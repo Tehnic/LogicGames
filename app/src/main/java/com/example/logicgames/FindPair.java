d8a96262fc25607bd32c2109312e0e647932333d
Tehnic <lemursltv@gmail.com>
2022-10-09 20:05:22 +0300

    Some more activities and design sketches

---
+++ app/src/main/java/com/example/logicgames/FindPair.java
@@ -0,0 +1,14 @@
+package com.example.logicgames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindPair extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pair);
+    }
+}
--- app/src/main/java/com/example/logicgames/MainActivity.java
+++ app/src/main/java/com/example/logicgames/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.logicgames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startButton, topButton, exitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.startButton);
+        topButton = (Button)findViewById(R.id.topButton);
+        exitButton = (Button)findViewById(R.id.exitButton);
+
+        startButton.setOnClickListener((View.OnClickListener) this);
+        topButton.setOnClickListener((View.OnClickListener) this);
+        exitButton.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.startButton) {
+            startActivity(new Intent(MainActivity.this, GamesList.class));
+        }
+        else if (view.getId() == R.id.topButton) {
+            //ToDo: add leaderboard from Google Play Games
+        }
+        else if (view.getId() == R.id.exitButton) {
+            finish();
+        }
     }
 }
