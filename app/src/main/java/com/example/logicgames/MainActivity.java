684a04c6fc670efc539bbfeb2ce1f68f209823c9
Tehnic <lemursltv@gmail.com>
2022-10-17 00:07:33 +0300

    Language change button (language not changing yet)

--- app/src/main/java/com/example/logicgames/MainActivity.java
+++ app/src/main/java/com/example/logicgames/MainActivity.java
@@ -1,27 +1,39 @@
 package com.example.logicgames;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button startButton, topButton, exitButton;
+    ImageView languageButton;
+    Configuration config;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        config = new Configuration(getResources().getConfiguration());
+        getResources().updateConfiguration(config,getResources().getDisplayMetrics());
         setContentView(R.layout.activity_main);
 
         startButton = (Button)findViewById(R.id.startButton);
         topButton = (Button)findViewById(R.id.topButton);
         exitButton = (Button)findViewById(R.id.exitButton);
-
-        startButton.setOnClickListener((View.OnClickListener) this);
-        topButton.setOnClickListener((View.OnClickListener) this);
-        exitButton.setOnClickListener((View.OnClickListener) this);
+        languageButton = (ImageView)findViewById(R.id.languageButton);
+        startButton.setOnClickListener(this);
+        topButton.setOnClickListener(this);
+        exitButton.setOnClickListener(this);
+        languageButton.setOnClickListener(this);
+        languageButton.setTag("en");
     }
 
     public void onClick(View view) {
@@ -33,6 +45,23 @@ public class MainActivity extends AppCompatActivity {
         }
         else if (view.getId() == R.id.exitButton) {
             finish();
+        }
+        else if (view.getId() == R.id.languageButton) {
+            languageButton.setOnClickListener(v -> {
+                if (languageButton.getTag().equals("en")) {
+                    languageButton.setImageResource(R.drawable.ru);
+                    languageButton.setTag("ru");
+                    config.locale = Locale.forLanguageTag("ru");
+                } else if (languageButton.getTag().equals("ru")) {
+                    languageButton.setImageResource(R.drawable.lv);
+                    languageButton.setTag("lv");
+                    config.locale = Locale.forLanguageTag("lv");
+                } else if (languageButton.getTag().equals("lv")) {
+                    languageButton.setImageResource(R.drawable.gb);
+                    languageButton.setTag("en");
+                    config.locale = Locale.forLanguageTag("en");
+                }
+            });
         }
     }
 }
